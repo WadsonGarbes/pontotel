69447b2a357d9fbc5a9a3653018cc50a189cabdc
Wadson Garbes <nenogarbes@hotmail.com>
2019-09-19 18:39:37 -0300

    Adiciona arquivo de configuração do app

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = 'pontotel+wadson=2019'
+    # em production é so alterar para postgres
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Cotações por página
+    CPP = 10
