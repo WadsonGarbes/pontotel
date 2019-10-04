2d5cef2f6919ba06b43640db977f818e9fc66e2f
Wadson Garbes <wadson.goncalves@fatec.sp.gov.br>
2019-10-04 20:05:04 -0300

    Adiciona URL do BD dependendo do ambiente

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = 'pontotel+wadson=2019'
     # em production é so alterar para postgres
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Cotações por página
     CPP = 10
