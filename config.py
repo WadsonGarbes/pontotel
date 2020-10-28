c930c2bc319a0e51ad9bdf59182382bd91002862
Wadson Garbes <wadson.goncalves@fatec.sp.gov.br>
2020-10-27 21:03:02 -0300

    update SECRET_KEY

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = 'pontotel+wadson=2019'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     # em production é so alterar para postgres
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     'sqlite:///' + os.path.join(basedir, 'app.db')
