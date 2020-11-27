c622f547cc1a3491691d4c651558d3175315102b
Wadson Garbes <nenogarbes@hotmail.com>
2020-11-27 17:16:34 -0300

    Adiciona sqlite

--- config.py
+++ config.py
@@ -5,8 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # em production é so alterar para postgres
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Cotações por página
     CPP = 10
